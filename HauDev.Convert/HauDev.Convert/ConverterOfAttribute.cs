2c2fdaa8c45ffa27e8cde47ae1215876c0de62c1
Nguyễn Văn Hậu <nguonsang@hotmail.com.vn>
2016-04-02 09:49:38 +0700

    a

---
+++ HauDev.Convert/HauDev.Convert/ConverterOfAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HauDev.Convert
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+    public class ConverterOfAttribute: Attribute
+    {
+        /// <summary>
+        /// Target => Kiểu dữ liệu mong muốn được thực hiện Convert
+        /// </summary>
+        public Type Target { set; get; }
+    }
+}
---
+++ HauDev.Convert/HauDev.Convert/HTypeCodeOfAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace HauDev.Convert
+{
+    /// <summary>
+    /// Mở rộng của kiểu dữ liệu
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field)]
+    public class HTypeCodeOfAttribute: Attribute
+    {
+        private Type type = null;
+        /// <summary>
+        /// 
+        /// </summary>
+        public Type Type
+        {
+            get { return type; }
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="type"></param>
+        public HTypeCodeOfAttribute(Type type)
+        {
+            this.type = type;
+        }
+    }
+}
